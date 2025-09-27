1dddf4bce0804f3811782e901cfa1ce5a101a68a
JVictorVI <jvictordomingos123@gmail.com>
2025-09-27 19:46:35 -0300

    Menu Inicial

---
+++ UnityUI/Assets/Scripts/MenuController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class MenuController : MonoBehaviour
+{
+
+    public VideoPlayer videoPlayer;
+    public RawImage exibidorVideo;
+
+    public GameObject mainMenu;
+
+    public Animator videoAnimator;
+
+    public AudioSource startGameSound;
+    public AudioSource mainMenuTheme;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        exibidorVideo.enabled = false;
+        mainMenu.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!videoPlayer.isPlaying && Input.anyKeyDown)
+        {
+            videoPlayer.Play();
+            startGameSound.Play();
+            videoAnimator.SetTrigger("StartBackgroundAnimation");
+            mainMenuTheme.Play();
+            exibidorVideo.enabled = true;
+            mainMenu.SetActive(true);
+        }
+    }
+}
