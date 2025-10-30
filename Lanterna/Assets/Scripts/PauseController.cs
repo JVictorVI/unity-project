57b5c778c5b09593b940e97b016945869f216abd
JVictorVI <jvictordomingos123@gmail.com>
2025-10-30 17:22:45 -0300

    Efeitos de blur no pause

--- Lanterna/Assets/Scripts/PauseController.cs
+++ Lanterna/Assets/Scripts/PauseController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 public class PauseController : MonoBehaviour
 {
@@ -7,16 +9,22 @@ public class PauseController : MonoBehaviour
     public AudioSource EnterPause;
     public AudioSource ExitPause;
 
+    public Volume volume;
+    private DepthOfField dof;
+
     public static bool isPaused { get; private set; } = false;
 
     void Start()
     {
         pauseMenuUI.SetActive(false);
+        volume.profile.TryGet(out dof);
+        dof.active = false;
+
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
-        {
+        {   
             if (isPaused)
                 Resume();
             else
@@ -30,11 +38,13 @@ public class PauseController : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1;
         isPaused = false;
+        dof.active = false;
     }
 
     void Pause()
     {
         pauseMenuUI.SetActive(true);
+        dof.active = true;
         Time.timeScale = 0;
         isPaused = true;
         EnterPause.Play();
