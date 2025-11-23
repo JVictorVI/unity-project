9169df027fc188e01be10e529083e2a3dc1a1beb
Amanda Evellin de Sousa Viana <amandasousav19@gmail.com>
2025-11-22 23:11:24 -0300

    Reenvio monstro
    novo monstro ja que o rogerio apagou o meu

---
+++ Monstro -O Segredo da Biblioteca Amanda/Assets/Scripts/HUD/GameOverManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public static GameOverManager Instance { get; private set; }
+    public GameObject gameOverPanel;
+    public bool pauseOnGameOver = true;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    public void ShowGameOver()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(true);
+
+        PauseController.isPaused = true;
+
+       // if (pauseOnGameOver)
+       //     Time.timeScale = 0f;
+    }
+
+    public void HideGameOver()
+    {
+        Cursor.visible = false;
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
+
+       // if (pauseOnGameOver)
+          //  Time.timeScale = 1f;
+    }
+
+    public void RestartScene()
+    {
+        //Time.timeScale = 1f;
+        PauseController.isPaused = false;
+
+        //"SceneManager.GetActiveScene().name"
+        SceneManager.LoadScene("LoadingScreen");
+    }
+}
